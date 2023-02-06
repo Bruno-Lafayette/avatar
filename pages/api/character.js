33996c0529e4e45a1a7622fee0a9cc9cc2b0ba7e
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-06 10:19:29 -0300

    first commit

---
+++ pages/api/character.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function character(req, res) {
+  res.status(200).json({ styles:  ["adventurer", "adventurerNeutral", "avataaars", "avataaarsNeutral", "bigEars", "bigEarsNeutral", "bigSmile", "bottts", "botttsNeutral", "croodles", "croodlesNeutral", "funEmoji", "thumbs", "icons", "identicon", "initials", "lorelei", "loreleiNeutral", "micah", "miniavs", "openPeeps", "personas", "pixelArt", "pixelArtNeutral", "shapes"] })
+}
---
+++ pages/api/type/adventurer.js
@@ -0,0 +1,16 @@
+// export default function character(req, res) {
+//     res.status(200).json({ styles:  ["adventurer", "adventurerNeutral", "avataaars", "avataaarsNeutral", "bigEars", "bigEarsNeutral", "bigSmile", "bottts", "botttsNeutral", "croodles", "croodlesNeutral", "funEmoji", "thumbs", "icons", "identicon", "initials", "lorelei", "loreleiNeutral", "micah", "miniavs", "openPeeps", "personas", "pixelArt", "pixelArtNeutral", "shapes"] })
+//   }
+
+
+
+export default function Options(req, res){
+
+    res.status(200).json({
+
+        eyes: ['variant26' , 'variant25' , 'variant24' , 'variant23' , 'variant22' , 'variant21' , 'variant20' , 'variant19' , 'variant18' , 'variant17' , 'variant16' , 'variant15' , 'variant14' , 'variant13' , 'variant12' , 'variant11' , 'variant10' , 'variant09' , 'variant08' , 'variant07' , 'variant06' , 'variant05' , 'variant04' , 'variant03' , 'variant02' , 'variant01']
+
+        
+
+    })
+}
